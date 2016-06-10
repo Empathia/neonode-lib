dc6b54ebc80eb631db53cb09ff3f11a53d6df090
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-10 11:51:59 -0500

    Improved support for logs

--- core/support/logger.js
+++ core/support/logger.js
@@ -1,36 +1,40 @@
+var colorize = process.env.WINSTON_COLORIZE || true;
 var winston = require('winston');
 
+/* global config */
+var transports = [
+  new winston.transports.File({
+    filename: config('logFile'),
+    handleExceptions: false,
+    colorize: false,
+    maxsize : 5242880,
+    maxFiles : 10,
+    json : true,
+  })
+];
+
+if (config('environment') !== 'test') {
+  transports.push(new winston.transports.Console({
+    handleExceptions: false,
+    json: false,
+    colorize : colorize,
+    timestamp : false,
+    stringify: true,
+    prettyPrint : true,
+    depth : null,
+    humanReadableUnhandledException : true,
+    showLevel : true,
+  }));
+}
+
 var logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      handleExceptions: false,
-      json: false,
-      colorize : true,
-      timestamp : false,
-      stringify: true,
-      prettyPrint : true,
-      depth : null,
-      humanReadableUnhandledException : true,
-      showLevel : true,
-
-    }),
-    new winston.transports.File({
-      filename: config('logFile'),
-      handleExceptions: false,
-      colorize: false,
-      maxsize : 5242880,
-      maxFiles : 10,
-      json : true,
-
-    })
-  ],
-  // exitOnError: false
+  transports: transports
 });
 
 module.exports = logger;
 
 module.exports.stream = {
-  write: function(message, encoding){
+  write: function(message){
     logger.info(message);
   }
 };
