b7e49fe3890b1dbb7c1feb4952963ccdcd0ad45e
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-14 11:17:48 -0500

    Fixed clc

--- core/support/lithium.js
+++ core/support/lithium.js
@@ -1,40 +1,35 @@
 var clc = require('cli-color');
 
-var dim = clc.blackBright,
-    yellow = clc.yellow,
-    white = clc.white,
-    red = clc.red;
-
 var high_level = 100,
     mid_level = 30,
     low_level = 5;
 
 function heat(level) {
   if (level > high_level) {
-    return red(level);
+    return clc.red(level);
   }
 
   if (level > mid_level) {
-    return yellow(level);
+    return clc.yellow(level);
   }
 
   if (level > low_level) {
-    return white(level);
+    return clc.white(level);
   }
 
-  return dim(level);
+  return clc.blackBright(level);
 }
 
 /* global logger, Li */
 Li.Engine.before.push(function beforeEngine(data) {
-  logger.info(dim('  -> ' + (data.spy.targetObject.className || data.spy.targetObject.constructor.className || '')  + '.' + data.spy.methodName));
+  logger.info(clc.blackBright('  -> ' + (data.spy.targetObject.className || data.spy.targetObject.constructor.className || '')  + '.' + data.spy.methodName));
 });
 
 Li.Engine.error.push(function errorEngine(data) {
-  logger.error(red('Lithium Detected an error...'));
+  logger.error(clc.red('Lithium Detected an error...'));
   logger.error(data.error.stack);
 });
 
 Li.Engine.after.push(function afterEngine(data) {
-  logger.info(dim('  <- ' + (data.spy.targetObject.className || data.spy.targetObject.constructor.className || '')  + '.' + data.spy.methodName + ' on ') + heat(data.time) + dim(' ms'));
+  logger.info(clc.blackBright('  <- ' + (data.spy.targetObject.className || data.spy.targetObject.constructor.className || '')  + '.' + data.spy.methodName + ' on ') + heat(data.time) + clc.blackBright(' ms'));
 });
