166726aaa0f3ae71ef5e8b57e83d5e03b195f84c
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-14 20:41:17 -0500

    Faux actions for missing methods

--- core/controllers/RestfulController.js
+++ core/controllers/RestfulController.js
@@ -1,3 +1,14 @@
-/* global Class, BaseController */
+/* global Class, NotFoundError, BaseController */
 module.exports = Class('RestfulController').inherits(BaseController)({
+  prototype: (function (actions) {
+    var props = {};
+
+    actions.forEach(function (action) {
+      props[action] = function (req, res, next) {
+        next(new NotFoundError('Neonode: handler for `' + this.constructor.className + '.' + action + '` is not implemented'));
+      }
+    });
+
+    return props;
+  })(['index', 'show', 'new', 'edit', 'update', 'create', 'destroy'])
 });
