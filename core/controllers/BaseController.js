de9e7716803a71eb8364d67f7751a3bf5276efd3
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-15 13:52:00 -0500

    Adds getName() helper to BaseController

--- core/controllers/BaseController.js
+++ core/controllers/BaseController.js
@@ -1,4 +1,9 @@
 /* global Class, CustomEventSupport */
 module.exports = Class('BaseController').includes(CustomEventSupport)({
-  layout: 'application'
+  layout: 'application',
+  prototype: {
+    getName: function () {
+      return this.constructor.className.replace('Controller', '');
+    }
+  }
 });
