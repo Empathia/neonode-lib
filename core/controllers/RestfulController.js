84679b1a553bf5d36a03f28c150a6e3b07137472
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-15 17:30:50 -0500

    Basic behaviour for RestfulController classes; fix

--- core/controllers/RestfulController.js
+++ core/controllers/RestfulController.js
@@ -1,14 +1,71 @@
-/* global Class, NotFoundError, BaseController */
+var aliases = {
+  form: ['update', 'edit', 'new']
+};
+
+function getAlias(name) {
+  return Object.keys(aliases).filter(function (key) {
+    return aliases[key].indexOf(name) > -1;
+  })[0];
+}
+
+/* global urlFor, Class, BaseController */
 module.exports = Class('RestfulController').inherits(BaseController)({
-  prototype: (function (actions) {
-    var props = {};
+  resources: [],
+  template: 'admin/index',
+  layout: 'admin',
+  scope: '',
+  prototype: {
+    getResources: function (currentUrl) {
+      var keys = this.constructor.scope.split('.');
+      var scope = urlFor;
 
-    actions.forEach(function (action) {
-      props[action] = function (req, res, next) {
-        next(new NotFoundError('Neonode: handler for `' + this.constructor.className + '.' + action + '` is not implemented'));
+      while (keys.length) {
+        scope = urlFor[keys.shift()];
       }
-    });
 
-    return props;
-  })(['index', 'show', 'new', 'edit', 'update', 'create', 'destroy'])
+      var fixedResources = this.constructor.resources.map(function (resourceName) {
+        return {
+          name: resourceName,
+          url: scope[resourceName].url()
+        };
+      });
+
+      return fixedResources.map(function (resource) {
+        resource.isActive = currentUrl.indexOf(resource.url) === 0;
+        return resource;
+      });
+    },
+    init: function () {
+      var resourceName = this.getName();
+
+      if (this.constructor.resources.indexOf(resourceName) === -1) {
+        return;
+      }
+
+      // decorate default behaviours
+      ['index', 'show', 'edit', 'new'].forEach(function (action) {
+        if (!this[action]) {
+          var fixedAction = getAlias(action) || action;
+
+          this[action] = function (req, res) {
+            res.render(this.constructor.template, {
+              resources: this.getResources(req.path),
+              resourceName: resourceName,
+              resourceParams: this.getParams && this.getParams(),
+              resourcePartial: resourceName.toLowerCase() + '/' + fixedAction
+            });
+          };
+        }
+      }, this);
+
+      // decorate non-get requests
+      ['update', 'create', 'destroy'].forEach(function (action) {
+        if (!this[action]) {
+          this[action] = function (req, res, next) {
+            next(new NotFoundError('Neonode: handler for `' + resourceName + '.' + action + '` is not implemented'));
+          };
+        }
+      }, this);
+    }
+  }
 });
