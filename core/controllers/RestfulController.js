839d4f9b94034d27f6babb37e55da3539e69ce3a
Alvaro Cabrera <pateketrueke@gmail.com>
2016-07-04 17:51:55 -0500

    Improved error-catching for inherited RestfulController actions; fix

--- core/controllers/RestfulController.js
+++ core/controllers/RestfulController.js
@@ -52,9 +52,26 @@ module.exports = Class('RestfulController').inherits(BaseController)({
             var _res = this.getResources(req.path);
             var _params = this.getParams && this.getParams(req, action);
 
+            var _failure = req.flash && req.flash()._failure || {};
+            var _err;
+
+            if (_failure.errors) {
+              _err = [];
+              _err.message = _failure.message || 'An error ocurred';
+
+              Object.keys(_failure.errors).forEach(function (key) {
+                _err.push({
+                  field: key,
+                  failure: _failure.errors[key]
+                });
+              });
+            }
+
             Promise.resolve(_params).then(function (fixedParams) {
               res.render(_tpl, {
+                errors: _err,
                 resources: _res,
+                resourceUrl: req.url,
                 resourceName: resourceName,
                 resourceParams: fixedParams,
                 resourcePartial: resourceName.toLowerCase() + '/' + fixedAction
@@ -69,6 +86,42 @@ module.exports = Class('RestfulController').inherits(BaseController)({
         if (!this[action]) {
           this[action] = function (req, res, next) {
             next(new NotFoundError('handler for `' + resourceName + '.' + action + '` is not implemented'));
+          };
+        } else {
+          var _action = this[action];
+
+          this[action] = function (req, res, next) {
+            var _url;
+
+            if (req.body) {
+              _url = req.body._url;
+              delete req.body._url;
+            }
+
+            var _result;
+
+            try {
+              _result = _action.call(this, req, res);
+            } catch (_e) {
+              return next(e);
+            }
+
+            Promise.resolve(_result).catch(function (error) {
+              if (req.flash) {
+                req.flash('_failure', {
+                  errors: error.errors,
+                  message: error.message
+                });
+              }
+
+              if (!res.finished) {
+                if (!_url) {
+                  next(error);
+                } else {
+                  res.redirect(_url);
+                }
+              }
+            });
           };
         }
       }, this);
