8ed9a77ee26980dec1b0b57b1904dd6293d32a9a
Alvaro Cabrera <pateketrueke@gmail.com>
2016-08-17 17:19:39 -0500

    Fixed API behavior; default renderer, _send() always return a Promise

--- core/mailers/BaseMailer.js
+++ core/mailers/BaseMailer.js
@@ -5,8 +5,19 @@ module.exports = Class('BaseMailer')({
   _transport: null,
   _options: null,
   _templates: null,
-  _renderer: null,
-  _extension: '',
+  _renderer: {
+    renderFile: function (file, vars) {
+      // default renderer is thulium
+      var partial = new Thulium({
+        template : fs.readFileSync(file).toString()
+      });
+
+      partial.parseSync().renderSync(vars || {});
+
+      return partial.view;
+    }
+  },
+  _extension: '.html',
 
   transport: function (transport) {
     if (transport) {
@@ -15,18 +26,14 @@ module.exports = Class('BaseMailer')({
       return transport;
     }
 
-    var klass = this;
+    var _klass = this;
+    var _transport;
 
-    while (klass && !klass._transport) {
-      var proto = klass.prototype;
-      klass = proto && proto.constructor;
-    }
+    do {
+      _transport = _klass._transport || _klass.prototype._transport || _klass.prototype.constructor._transport;
+    } while (_klass && _klass !== this && !_transport)
 
-    if (klass && klass._transport) {
-      return klass && klass._transport;
-    }
-
-    throw new Error(this.className + ' can\'t find a nodemailer transport');
+    return _transport;
   },
 
   setMethodTemplate: function (methodName, templateName) {
@@ -49,22 +56,25 @@ module.exports = Class('BaseMailer')({
 
   setRendererEngine: function (engine, extName) {
     this._renderer = engine;
-    this._extension = extName ? '.' + extName : '';
+
+    if (extName) {
+      this._extension = (extName.indexOf('.') > -1 ? '' : '.') + extName;
+    }
+
+    return this;
   },
 
-  _send: function (methodName) {
-    var args = Array.prototype.slice.call(arguments, 1);
+  _send: function (methodName, recipients, localVars) {
+    recipients = recipients || [];
+    localVars = localVars || {};
+
+    var defaultOptions = this._options || {};
+    var localOptions = localVars.options || {};
+    var options = {};
     var template;
 
-    var defaultOptions = this._options;
-
-    var recipients = args[0];
-    var localVars = args[1];
-
-    var options = {};
-
     Object.keys(defaultOptions).forEach(function (key) {
-      options[key] = localVars._options[key] || defaultOptions[key];
+      options[key] = typeof localOptions[key] === 'undefined' ? defaultOptions[key] : localOptions[key];
     });
 
     if (this._templates && this._templates[methodName].template) {
@@ -79,18 +89,28 @@ module.exports = Class('BaseMailer')({
       methodName + this._extension
     );
 
-    try {
-      fs.accessSync(conventionalTemplate, fs.F_OK);
-      template = conventionalTemplate;
-    } catch (e) {
-      throw new Error('Method ' + methodName + ' in ' + this.className + ' doesn\'t have a template');
-    }
+    var self = this;
 
-    var html = this._renderer.renderFile(template, localVars);
+    return new Promise(function (resolve) {
+      var _transport = self.transport();
 
-    options.html = html;
-    options.to = recipients;
+      if (!_transport) {
+        throw new Error((self.className || self.name || 'BaseMailer') + ' can\'t find a nodemailer transport');
+      }
 
-    return this.transport().sendMail(options);
+      try {
+        fs.accessSync(conventionalTemplate, fs.F_OK);
+        template = conventionalTemplate;
+      } catch (e) {
+        throw new Error('Method ' + methodName + ' in ' + this.className + ' doesn\'t have a template');
+      }
+
+      var html = self._renderer.renderFile(template, localVars);
+
+      options.html = html;
+      options.to = recipients;
+
+      resolve(_transport.sendMail(options));
+    });
   },
 });
