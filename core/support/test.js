3ed9f7877f9614add13a289fa607fc24b010cb70
Alvaro Cabrera <pateketrueke@gmail.com>
2016-08-18 13:23:20 -0500

    Fixed fetch() behavior with passed data

--- core/support/test.js
+++ core/support/test.js
@@ -1,3 +1,5 @@
+/* globals config, Neonode */
+
 require('..');
 
 var sa = require('superagent');
@@ -19,15 +21,15 @@ Neonode._initialize(function () {
   var filter = process.argv.slice(2)[0] || '.js';
 
   Neonode._util.glob('test/**/*.js')
-    .forEach((file) => {
+    .forEach(function (file) {
       if (file.toLowerCase().indexOf(filter) > -1) {
         mocha.addFile(file);
       }
     });
 
   // run Mocha
-  mocha.run((failures) => {
-    process.on('exit', () => {
+  mocha.run(function (failures) {
+    process.on('exit', function () {
       process.exit(failures);
     });
 
@@ -38,55 +40,68 @@ Neonode._initialize(function () {
 
 // common helper
 function fetch(resource) {
-  var args = Array.prototype.slice.call(arguments, 1);
-
-  var _promise = new Bluebird(function (resolve, reject) {
-    var url = config('siteURL') + resource.url.call(null, args);
-
-    sa.agent()[resource.verb](url)
-      .set('Accept', 'text/html')
-      .end(function (err, res) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(res);
-        }
-      });
-  });
-
+  var _promise = null;
   var _stack = [];
+  var _data;
 
-  var _it = function (done) {
-    try {
-      _promise.then(function (res) {
-        _stack.forEach(function (cb) {
-          cb(res);
+  var args = Array.prototype.slice.call(arguments, 1);
+  var url = config('siteURL') + resource.url.call(null, args);
+
+  function load() {
+    if (!_promise) {
+      _promise = new Bluebird(function (resolve, reject) {
+        var req = sa.agent()[resource.verb](url)
+          .set('Accept', 'text/html');
+
+        if (_data) {
+          req.send(typeof _data === 'function' ? _data(req) : _data);
+        }
+
+        return req.end(function (err, res) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(res);
+          }
         });
-
-        done();
       });
-    } catch (e) {
-      done(e);
     }
-  };
 
-  _it.ok = function (_code) {
+    return _promise;
+  }
+
+  function it() {
+    return load().then(function (res) {
+      _stack.forEach(function (cb) {
+        cb(res);
+      });
+
+      return res;
+    });
+  }
+
+  it.ok = function (code) {
     _stack.push(function (res) {
       expect(res.ok).to.equal(true);
-      expect(res.status).to.equal(_code || 200);
+      expect(res.status).to.equal(code || 200);
     });
 
-    return _it;
+    return it;
   };
 
-  _it.fail = function (_code) {
+  it.err = function (code) {
     _stack.push(function (res) {
       expect(res.error).to.equal(true);
-      expect(res.status).to.equal(_code || 500);
+      expect(res.status).to.equal(code || 500);
     });
 
-    return _it;
+    return it;
   };
 
-  return _it;
-};
+  it.with = function (data) {
+    _data = data;
+    return it;
+  };
+
+  return it;
+}
